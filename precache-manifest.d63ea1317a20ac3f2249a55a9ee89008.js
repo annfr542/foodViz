1f4f01a839fc6ffb8380687dcf20a7900d71dedb
Anna <annfr542@student.liu.se>
2020-08-31 17:38:52 +0200

    Updates

---
+++ precache-manifest.d63ea1317a20ac3f2249a55a9ee89008.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7897114161503ae3f30d931a6be8f102",
+    "url": "/foodViz/index.html"
+  },
+  {
+    "revision": "da8e85a4f9ffad9f53a2",
+    "url": "/foodViz/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9ab2c5ff3b6295bffd93",
+    "url": "/foodViz/static/css/main.0a9a864e.chunk.css"
+  },
+  {
+    "revision": "da8e85a4f9ffad9f53a2",
+    "url": "/foodViz/static/js/2.b3e736d1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/foodViz/static/js/2.b3e736d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ab2c5ff3b6295bffd93",
+    "url": "/foodViz/static/js/main.3b0b0429.chunk.js"
+  },
+  {
+    "revision": "64641653d95d94bf9d04",
+    "url": "/foodViz/static/js/runtime-main.e6c9aaad.js"
+  }
+]);
